06aa5279cb8e40c0e6e99c0833aaba0be422f1d6
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-06 16:17:53 +1000

    styling and bug fix

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,13 +4,22 @@ import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((video, index) => (
-        <Box key={index}>
-          {video.id.videoId && <VideoCard video={video} />}
-          {video.id.channelId && <ChannelCard channelDetail={video} />}
-        </Box>
-      ))}
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map(
+        (video, index) =>
+          !video.id.playlistId && (
+            <Box key={index}>
+              {video.id.videoId && <VideoCard video={video} />}
+              {video.id.channelId && <ChannelCard channelDetail={video} />}
+            </Box>
+          )
+      )}
     </Stack>
   );
 };
